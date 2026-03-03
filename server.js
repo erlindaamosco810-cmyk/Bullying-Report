62b2c8bd38b346a68acb158088112e2fc0bb9d65
erlindaamosco810-cmyk <erlindaamosco810@gmail.com>
2026-03-03 12:34:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-import "./logger.js";
-import { at as _createServer, ct as resolveServerOptions, lt as restartServerWithUrls, ot as createServer, st as createServerCloseFn, ut as serverConfigDefaults } from "./config.js";
+import express from "express";
+import dotenv from "dotenv";
 
-export { createServer };
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Example middleware
+app.use(express.json());
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// Use PORT from environment, default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
